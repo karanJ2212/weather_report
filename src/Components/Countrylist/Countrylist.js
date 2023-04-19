9ad6b109bcf06c9f00b9d465847989476c364a2c
karan jain <karanjain2212@gmail.com>
2023-04-19 10:41:53 +0530

    setup and navbar

---
+++ src/Components/Countrylist/Countrylist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Countrycard from './Countrycard';
+
+export default function Countrylist() {
+  return (
+    <div>
+      countrylist
+      <Countrycard />
+    </div>
+  );
+}
---
+++ src/Components/States/Statelist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Statecard from './Statecard';
+
+export default function Statelist() {
+  return (
+    <div>
+      Statelist
+      <Statecard />
+    </div>
+  );
+}
