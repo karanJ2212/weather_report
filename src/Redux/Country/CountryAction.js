73984630707afe6639bdc974ac172ae748b30ef9
karan jain <karanjain2212@gmail.com>
2023-04-20 22:56:41 +0530

    comments fixed

--- src/Redux/Country/CountryAction.js
+++ src/Redux/Country/CountryAction.js
@@ -1,20 +1,3 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// const fetchCountry = createAsyncThunk(
-//   'countries/fetchCountry',
-//   async () => {
-//     const response = await axios.get('https://countriesnow.space/api/v0.1/countries/states');
-//     const countriesData = response.data.data.reduce((accumulator, country) => {
-//       const updatedCountries = { ...accumulator };
-//       updatedCountries[country.name] = country;
-//       return updatedCountries;
-//     }, {});
-//     return countriesData;
-//   },
-// );
-// export default fetchCountry;
-/// ////////////////////////////////////////////////////////////////
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const fetchCountry = createAsyncThunk(
