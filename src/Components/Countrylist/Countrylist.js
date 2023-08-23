4d42f8b52b20406f946e24c708490fb2d27a6f57
karan jain <karanjain2212@gmail.com>
2023-08-23 16:21:30 +0530

    search functionality added

--- src/Components/Countrylist/Countrylist.js
+++ src/Components/Countrylist/Countrylist.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Countrycard from './Countrycard';
-import fetchCountry from '../../Redux/Country/CountryAction';
+import {
+  fetchCountry,
+  fetchSingleCountry,
+} from '../../Redux/Country/CountryAction';
 import './Countrylist.css';
 
 export default function Countrylist() {
   const dispatch = useDispatch();
+  const [selectedCountry, setSelectedCountry] = useState(''); // State for selected country
 
   useEffect(() => {
     dispatch(fetchCountry());
@@ -14,29 +18,56 @@ export default function Countrylist() {
   const countries = useSelector((state) => state.countries.countries);
   const status = useSelector((state) => state.countriesStatus);
 
+  const handleCountrySelect = (countryName) => {
+    setSelectedCountry(countryName);
+    dispatch(fetchSingleCountry(countryName));
+  };
+
   if (status === 'loading') {
-    return (
-      <div>Loading...</div>
-    );
+    return <div>Loading...</div>;
   }
   return (
     <div className="countries-section" data-testid="countries-wrapper">
       <div className="header-section">
         <h1 className="title-header">World Weather</h1>
         <p className="choose-country">Choose a country</p>
+
+        {/* drop down */}
+        <select
+          className="country-dropdown"
+          value={selectedCountry}
+          onChange={(e) => handleCountrySelect(e.target.value)}
+        >
+          <option value="">Select a country</option>
+          {Object.keys(countries).map((countryName) => (
+            <option key={countryName} value={countryName}>
+              {countryName}
+            </option>
+          ))}
+        </select>
+
         <div className="country-list-header">
           <h2 className="country-list-title">Countries</h2>
         </div>
       </div>
       <div className="country-card-container">
-        {Object.values(countries).map((country) => (
+        {selectedCountry ? (
           <Countrycard
-            key={country.iso3}
-            name={country.name}
-            states={country.states}
-            iso3={country.iso3}
+            key={countries[selectedCountry].iso3}
+            name={countries[selectedCountry].name}
+            states={countries[selectedCountry].states}
+            iso3={countries[selectedCountry].iso3}
           />
-        ))}
+        ) : (
+          Object.values(countries).map((country) => (
+            <Countrycard
+              key={country.iso3}
+              name={country.name}
+              states={country.states}
+              iso3={country.iso3}
+            />
+          ))
+        )}
       </div>
     </div>
   );
